84c2585cd731acba43a0b8e337f30a7386d94a95
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-09-28 03:54:41 +0200

    Intermediate, reactivated dart_domain_locality_t.host

--- dart-impl/base/src/internal/domain_locality.c
+++ dart-impl/base/src/internal/domain_locality.c
@@ -597,6 +597,7 @@ dart_ret_t dart__base__locality__domain__create_subdomains(
                        "could not get host name of node %d",
                        domain->relative_index);
       } else {
+        strcpy(domain->host, node_hostname);
         if (dart__base__host_topology__num_node_modules(
               host_topology,
 //            domain->host,
@@ -624,6 +625,7 @@ dart_ret_t dart__base__locality__domain__create_subdomains(
                        "could not get host name of node %d",
                        domain->parent->relative_index);
       } else {
+        strcpy(domain->host, node_hostname);
         if (dart__base__host_topology__node_module(
               host_topology,
 //            domain->host,
