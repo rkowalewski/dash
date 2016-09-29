b7b38860e657eef66e1eaab56d76cf7b054cf0ff
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-09-29 04:24:44 +0200

    Removed debug switch

--- dart-impl/base/src/internal/unit_locality.c
+++ dart-impl/base/src/internal/unit_locality.c
@@ -231,12 +231,12 @@ dart_ret_t dart__base__unit_locality__local_unit_new(
   loc->team   = team;
   loc->hwinfo = *hwinfo;
 
-#if 1 || defined(DART__BASE__LOCALITY__SIMULATE_MICS)
+#if defined(DART__BASE__LOCALITY__SIMULATE_MICS)
   /* Assigns every second unit to a MIC host name.
    * Useful for simulating a heterogeneous node-level topology
    * for debugging.
    */
-  if (myid % 2 == 1) {
+  if (myid % 3 == 1) {
     strncat(loc->hwinfo.host, "-mic0", 5);
   }
 #endif
